eaf5f460ec73cfb50fbee6cd79c6658b1fed8084
daavoo <daviddelaiglesiacastro@gmail.com>
2025-08-12 16:50:48 +0200

    Fix lint

--- src/any_guardrail/guardrails/injec_guard.py
+++ src/any_guardrail/guardrails/injec_guard.py
@@ -19,7 +19,7 @@ class InjecGuard(HuggingFace):
     SUPPORTED_MODELS: ClassVar = ["leolee99/InjecGuard"]
 
     def _load_model(self) -> None:
-        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_id, trust_remote_code=True)  # type: ignore[arg-type]
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_id, trust_remote_code=True)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)  # type: ignore[no-untyped-call]
 
     def _post_processing(self, model_outputs: dict[str, Any]) -> GuardrailOutput:
