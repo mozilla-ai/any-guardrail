8bf1991bf132a66ab4f0090a037922700dbe678b
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2025-08-12 09:58:31 +0200

    Update src/any_guardrail/guardrails/protectai.py
    Co-authored-by: Nathan Brake <33383515+njbrake@users.noreply.github.com>

--- src/any_guardrail/guardrails/protectai.py
+++ src/any_guardrail/guardrails/protectai.py
@@ -12,7 +12,7 @@ class Protectai(Guardrail):
     """Prompt injection detection encoder based models.
 
     For more information, please see the model cards:
-    [ProtectA](https://huggingface.co/collections/protectai/llm-security-65c1f17a11c4251eeab53f40).
+    [ProtectAI](https://huggingface.co/collections/protectai/llm-security-65c1f17a11c4251eeab53f40).
 
     Args:
         model_id: HuggingFace path to model.
