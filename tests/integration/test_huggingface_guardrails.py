76fbaa32df889bc433752b76d9ea58afc1c07ccd
Daniel Nissani <daniel@mozilla.ai>
2025-10-21 13:28:53 -0400

    Adding Llama Guard to any-guardrail (#89)
    * Updated docs to include Llama Guard.
    
    * Updated codebase to support llama guards 3 & 4. They require different utilities from HuggingFace. They are similarly organized to the off topic model.
    
    * Updated unit tests to support how the llama guard files are organized.
    
    * Small edit to test.
    
    * Nit tests.
    
    * Linter finally started working. This is becoming a persistent issue.
    
    * Add LLAMA_GUARD to test_huggingface
    
    * Everything should be funcitonal now. Consolidate the logic for all llama guards into one file. Found some hairy things with jinja templates that had to implement some more if/else logic.
    
    * Forgot to remove references to LlamaGuard4 and LlamaGuard3
    
    * Removed last reference.
    
    * Lint
    
    * Addressed David's comments
    
    ---------
    
    Co-authored-by: dni138 <Daniel Nissani>
    Co-authored-by: David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
    Co-authored-by: Daniel Nissani <danielnissani@danielnissani.taild6f0e.ts.net>

--- tests/integration/test_huggingface_guardrails.py
+++ tests/integration/test_huggingface_guardrails.py
@@ -16,6 +16,7 @@ from any_guardrail.guardrails.huggingface import HuggingFace
         (GuardrailName.INJECGUARD, {}, None),
         (GuardrailName.JASPER, {}, None),
         (GuardrailName.PANGOLIN, {}, None),
+        (GuardrailName.LLAMA_GUARD, {"model_id": "hf-internal-testing/tiny-random-LlamaForCausalLM"}, None),
         # (GuardrailName.PROTECTAI, {}, None), # Requires HF login
         # (GuardrailName.SENTINEL, {}, None),  # Requires HF login
         # (GuardrailName.SHIELD_GEMMA, {"policy": "Do not provide harmful or dangerous information"}, None),  # Requires HF login
@@ -28,10 +29,15 @@ def test_huggingface_guardrails(
     guardrail = AnyGuardrail.create(guardrail_name=guardrail_name, **guardrail_kwargs)
 
     assert isinstance(guardrail, HuggingFace)
-    assert guardrail.model_id == guardrail.SUPPORTED_MODELS[0]
+    assert guardrail.model_id == (guardrail_kwargs.get("model_id") or guardrail.SUPPORTED_MODELS[0])
 
     result = guardrail.validate("What is the weather like today?")
 
     assert result.valid
-    assert result.explanation == expected_explanation
-    assert result.score is not None
+
+    if guardrail_name == GuardrailName.LLAMA_GUARD:
+        assert result.explanation is not None
+        assert result.score is None
+    else:
+        assert result.explanation == expected_explanation
+        assert result.score is not None
